bb84dba39b50270584a906bbd755b4e14bff912f
Arogunmasa Abayomi <nomorelimit11@gmail.com>
2024-07-30 01:31:31 +0100

    added some functionalities to the menu button

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const header = document.querySelector("header");
+const menuBtn = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav__links");
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("scroll", window.scrollY);
+});
+
+menuBtn.addEventListener("click", () => {
+  if (menuBtn) {
+    menuBtn.classList.toggle("is-active");
+    menu.classList.toggle("active");
+  }
 });
