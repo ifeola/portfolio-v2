ff514af67ea816348caefea2206bfbe569fd5cd6
Arogunmasa Abayomi <nomorelimit11@gmail.com>
2024-07-30 22:15:55 +0100

    Commiting new files and changes. Added the project and contact sections

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const header = document.querySelector("header");
 const menuBtn = document.querySelector(".hamburger");
-const menu = document.querySelector(".nav__links");
 
+//
 window.addEventListener("scroll", () => {
   header.classList.toggle("scroll", window.scrollY);
 });
 
 menuBtn.addEventListener("click", () => {
-  if (menuBtn) {
-    menuBtn.classList.toggle("is-active");
-    menu.classList.toggle("active");
+  if (menuBtn.getAttribute("aria-expanded") == "false") {
+    menuBtn.setAttribute("aria-expanded", "true");
+  } else {
+    menuBtn.setAttribute("aria-expanded", "false");
   }
 });
